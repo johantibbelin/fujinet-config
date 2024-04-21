1d63ff67150bc00ffcfe2445ae24eb077f9d670b
Johan Tibbelin <42235680+johantibbelin@users.noreply.github.com>
2024-04-21 10:57:40 +0200

    A few screen tests added

---
+++ src/atari16bit/tests/test_screen.c
@@ -0,0 +1,11 @@
+#ifdef BUILD_ATARI16BIT
+#include "../screen.h"
+
+void test_screen_functions()
+{
+    printf("Test of screen functions, this shold not be seen.\n");
+    screen_clrscr();
+    printf("This is shown after screen is cleared.\n\r");
+    screen_print_inverse("This is in inverse.\n\r");
+}
+#endif /* BUILD_ATARI16BIT */
