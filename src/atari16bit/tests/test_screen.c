90e505444c5b7e1ba10d487c6b368ee640a85008
Johan Tibbelin <42235680+johantibbelin@users.noreply.github.com>
2024-04-21 14:25:44 +0200

    Fixed Gotoxy

--- src/atari16bit/tests/test_screen.c
+++ src/atari16bit/tests/test_screen.c
@@ -7,5 +7,9 @@ void test_screen_functions()
     screen_clrscr();
     printf("This is shown after screen is cleared.\n\r");
     screen_print_inverse("This is in inverse.\n\r");
+    screen_gotoxy(5,5);
+    printf("5,5");
+    screen_gotoxy(10,10);
+    screen_print_inverse("10,10");
 }
 #endif /* BUILD_ATARI16BIT */
